56e32d47d2383e9c4ef2207aee5c521c95606e07
Celine Johnson <johnsonceline670@gmail.com>
2019-07-31 10:00:57 +0200

    loginreg

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: 'hotel-create', loadChildren: './pages/hotel-create/hotel-create.module#HotelCreatePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6p7V4F2FC9vqJl9Ghm52JOuFfNB13gAA",
+    authDomain: "hotelapp-50ade.firebaseapp.com",
+    databaseURL: "https://hotelapp-50ade.firebaseio.com",
+    projectId: "hotelapp-50ade",
+    storageBucket: "",
+    messagingSenderId: "231786139915",
+    appId: "1:231786139915:web:c333c4e2bfd6a388"
+  };
+
+  export default firebaseConfig
